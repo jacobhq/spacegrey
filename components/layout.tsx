f477e295155b0d7509178de586a79961768dfe1d
jacobhq <jacob.marshall.jem@gmail.com>
2022-01-21 21:04:20 +0000

    ✨ Donate modal

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,5 @@
 import { HamburgerIcon, MoonIcon, SunIcon } from '@chakra-ui/icons'
-import { Container, Heading, HStack, IconButton, useColorMode, Text, Box, ButtonGroup, MenuButton, Menu, MenuList, MenuItem, Skeleton, Tooltip, MenuDivider, Modal, Button, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure, VStack, useToast } from '@chakra-ui/react'
+import { Container, Heading, HStack, IconButton, useColorMode, Text, Box, ButtonGroup, MenuButton, Menu, MenuList, MenuItem, Skeleton, Tooltip, MenuDivider, Modal, Button, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure, VStack, useToast, Link, Slider, SliderFilledTrack, SliderThumb, SliderTrack } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { ReactNode, useState } from 'react'
@@ -19,6 +19,7 @@ export default function Layout({ children, title }: layoutProps) {
   const [isLoading, setLoading] = useState(false)
   const router = useRouter()
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen: isDonateOpen, onOpen: onDonateOpen, onClose: onDonateClose } = useDisclosure()
   const toast = useToast()
 
   function authClick() {
@@ -35,6 +36,8 @@ export default function Layout({ children, title }: layoutProps) {
     setLoading(true)
     return router.push('/api/auth/logout')
   }
+
+  let [num, setNum] = useState(3)
 
   return (
     <div>
@@ -85,7 +88,32 @@ export default function Layout({ children, title }: layoutProps) {
                 </Modal>
                 <MenuItem onClick={logout}>Logout</MenuItem>
                 <MenuDivider />
-                <MenuItem>Donate to JacobHQ</MenuItem>
+                <MenuItem onClick={onDonateOpen}>Donate to JacobHQ</MenuItem>
+                <Modal isOpen={isDonateOpen} onClose={onDonateClose}>
+                  <ModalOverlay />
+                  <ModalContent>
+                    <ModalHeader></ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                      <Heading>{num}</Heading>
+                      <Text>{num > 1 ? 'coffees' : 'coffee'}</Text>
+                      <Slider aria-label='slider-ex-1' defaultValue={3} min={1} max={10} onChange={(val) => setNum(val)}>
+                        <SliderTrack>
+                          <SliderFilledTrack />
+                        </SliderTrack>
+                        <SliderThumb />
+                      </Slider>
+                    </ModalBody>
+
+                    <ModalFooter>
+                      <ButtonGroup>
+                        <Link href={`https://buy.jacob.omg.lol/donate/${num}`} _hover={{ testDecoration: 'none' }}>
+                          <Button colorScheme="blue">Donate {num} {num > 1 ? 'coffees' : 'coffee'}</Button>
+                        </Link>
+                      </ButtonGroup>
+                    </ModalFooter>
+                  </ModalContent>
+                </Modal>
               </MenuList>
             </Menu>
             <Tooltip label={'Set theme to'.concat(' ', colorMode === 'light' ? 'dark' : 'light')} aria-label={'Set theme to'.concat(' ', colorMode === 'light' ? 'dark' : 'light')}>
