63f3a4c74185cec3f0a44874723bd78af8d5538a
jacobhq <jacob.marshall.jem@gmail.com>
2022-02-02 15:07:09 +0000

    🚀 Add wishlist api route

--- pages/api/addWishlistItem/[...slug].ts
+++ pages/api/addWishlistItem/[...slug].ts
@@ -1,6 +1,36 @@
+import { getSession } from '@auth0/nextjs-auth0'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../lib/prisma'
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
     const { slug } = req.query
-    res.end(`Post: ${slug.join(', ')}`)
+    const session = getSession(req, res)
+    const userExists = await prisma.user.findMany({
+        where: {
+            auth0Id: session?.user.sub
+        }
+    })
+
+    if (!userExists) {
+        await prisma.user.create({
+            data: {
+                auth0Id: session?.user.sub
+            }
+        })
+    }
+
+    const user = await prisma.user.findMany({
+        where: {
+            auth0Id: session?.user.sub
+        }
+    })
+
+    const create = await prisma.wishlistItem.create({
+        data: {
+            User: { connect: { id: user[0].id } },
+            Product: { connect: { id: parseInt(slug[0]) } }
+        }
+    })
+
+    res.status(201).send('Item created')
 }
