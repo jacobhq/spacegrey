415e328a89fc77f41130f1b524c117d03072f7fb
jacobhq <jacobm0@glynschool.org>
2022-02-12 11:24:28 +0000

    💄 Fix wishlist layout

--- pages/wishlist.tsx
+++ pages/wishlist.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Layout from '../components/layout'
-import { Box, Skeleton, Text } from '@chakra-ui/react'
+import { Box, Skeleton, Spinner, Text } from '@chakra-ui/react'
 import * as React from 'react'
 import { ProductCard } from '../components/productCard'
 import { ProductGrid } from '../components/productGrid'
@@ -8,21 +8,11 @@ import { useUser } from '@auth0/nextjs-auth0'
 import prisma from '../lib/prisma'
 import useSWR from 'swr'
 
-function List(products: any) {
+export default function Home({ products }: any): any {
+    const { user, error: userError, isLoading: authLoading } = useUser()
     // @ts-ignore
     const fetcher = (...args) => fetch(...args).then(res => res.json())
     const { data, error } = useSWR('/api/wishlist', fetcher)
-
-    if (error) return <Text>Error while loading, email bugs@jacob.omg.lol</Text>
-    if (!data) return <Skeleton />
-
-    return data.map((product: { id: string; name: string; currency: string; price: number; salePrice: number; flag: string; imageUrl: string; rating: number; ratingCount: number; description: string; images: { id: string; src: string; alt: string }[]; buyUrl: string; marketplace: string } | { id: string; name: string; currency: string; price: number; imageUrl: string; rating: number; ratingCount: number; description: string; images: { id: string; src: string; alt: string }[]; buyUrl: string; marketplace: string; salePrice?: undefined; flag?: undefined }) => (
-        <ProductCard key={product.id} product={product} />
-    ))
-}
-
-export default function Home({ products }: any): any {
-    const { user, error, isLoading: authLoading } = useUser()
 
     return (
         <div>
@@ -35,7 +25,11 @@ export default function Home({ products }: any): any {
                         py={{ base: '6', md: '8', lg: '12' }}
                     >
                         <ProductGrid>
-                            <List products={products} />
+                            {data && data.map((product: { id: string; name: string; currency: string; price: number; salePrice: number; flag: string; imageUrl: string; rating: number; ratingCount: number; description: string; images: { id: string; src: string; alt: string }[]; buyUrl: string; marketplace: string } | { id: string; name: string; currency: string; price: number; imageUrl: string; rating: number; ratingCount: number; description: string; images: { id: string; src: string; alt: string }[]; buyUrl: string; marketplace: string; salePrice?: undefined; flag?: undefined }) => (
+                                <ProductCard key={product.id} product={product} />
+                            ))}
+                            {!data && !error && <Spinner />}
+                            {error && <Text>Error while loading, email jacob@omg.lol</Text>}
                         </ProductGrid>
                     </Box>
                 </main>
