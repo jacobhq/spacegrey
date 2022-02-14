ed1acdbfcc1cd203bd7ebeea6d1e8521920b3991
jacobhq <jacobm0@glynschool.org>
2022-02-14 18:10:18 +0000

    📈 More splitbee

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,17 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { UserProvider } from '@auth0/nextjs-auth0';
 import theme from '../lib/theme'
+import splitbee from '@splitbee/web';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    splitbee.init({
+      scriptUrl: "/bee.js",
+      apiUrl: "/_hive",
+    })
+  }, [])
+
   return (
     <UserProvider>
       <ChakraProvider theme={theme}>
