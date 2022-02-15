44d1181d295a421e0a094bb82b67c90e1fcd35a8
jacobhq <jacobm0@glynschool.org>
2022-02-15 22:25:05 +0000

    ✨ Filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,39 @@
 import type { NextPage } from 'next'
 import Layout from '../components/layout'
-import { Box } from '@chakra-ui/react'
+import { Box, Button, Menu, MenuButton, MenuDivider, MenuItem, MenuItemOption, MenuList, MenuOptionGroup } from '@chakra-ui/react'
 import * as React from 'react'
 import { ProductCard } from '../components/productCard'
 import { ProductGrid } from '../components/productGrid'
-import { useUser } from '@auth0/nextjs-auth0'
+import { getSession, useUser } from '@auth0/nextjs-auth0'
 import { prisma } from '../lib/prisma'
+import { FaFilter } from 'react-icons/fa'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
-export default function Home ({products}: any): any {
+export default function Home({ products, matchedProducts }: any): any {
   const { user, error, isLoading: authLoading } = useUser()
+  const router = useRouter()
+  let [filter, setFilter] = useState('all')
+  let computed = filter === "all" ? products : matchedProducts
 
   return (
     <div>
       <Layout>
         <main>
+          <Menu closeOnSelect={false}>
+            <MenuButton as={Button} leftIcon={<FaFilter />}>
+              Filter
+            </MenuButton>
+            <MenuList>
+              {/* @ts-ignore */}
+              <MenuOptionGroup value={filter} onChange={(e) => setFilter(e)} type='radio'>
+                <MenuItemOption value='all'>Show all</MenuItemOption>
+                <MenuItemOption value='match'>Color matched only</MenuItemOption>
+              </MenuOptionGroup>
+              <MenuDivider />
+              <MenuItem onClick={() => router.push('/wishlist')} isDisabled={!user}>Wishlist only</MenuItem>
+            </MenuList>
+          </Menu>
           <Box
             maxW="7xl"
             mx="auto"
@@ -21,7 +41,7 @@ export default function Home ({products}: any): any {
             py={{ base: '6', md: '8', lg: '12' }}
           >
             <ProductGrid>
-              {products.map((product: { id: string; name: string; currency: string; price: number; salePrice: number; flag: string; imageUrl: string; rating: number; ratingCount: number; description: string; images: { id: string; src: string; alt: string }[]; buyUrl: string; marketplace: string } | { id: string; name: string; currency: string; price: number; imageUrl: string; rating: number; ratingCount: number; description: string; images: { id: string; src: string; alt: string }[]; buyUrl: string; marketplace: string; salePrice?: undefined; flag?: undefined }) => (
+              {computed.map((product: { id: string; name: string; currency: string; price: number; salePrice: number; flag: string; imageUrl: string; rating: number; ratingCount: number; description: string; images: { id: string; src: string; alt: string }[]; buyUrl: string; marketplace: string } | { id: string; name: string; currency: string; price: number; imageUrl: string; rating: number; ratingCount: number; description: string; images: { id: string; src: string; alt: string }[]; buyUrl: string; marketplace: string; salePrice?: undefined; flag?: undefined }) => (
                 // @ts-ignore
                 <ProductCard key={product.id} product={product} />
               ))}
@@ -33,7 +53,13 @@ export default function Home ({products}: any): any {
   )
 }
 
-export const getServerSideProps = async ({}) => {
+export const getServerSideProps = async () => {
   const products = await prisma.product.findMany()
-  return { props: { products } }
+  const matchedProducts = await prisma.product.findMany({
+    where: {
+      verifiedColorMatch: true
+    }
+  })
+
+  return { props: { products, matchedProducts } }
 }
