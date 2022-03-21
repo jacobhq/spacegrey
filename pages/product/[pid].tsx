46a17c47d269c5400da2f9990093d92653dd21a7
jacobhq <jacob@omg.lol>
2022-03-21 09:39:05 +0000

    🚧 Product page

--- pages/product/[pid].tsx
+++ pages/product/[pid].tsx
@@ -1,17 +1,170 @@
+import { useUser } from '@auth0/nextjs-auth0'
+import { Container, Text, Box, List, Image, SimpleGrid, Flex, Stack, Heading, useColorModeValue, StackDivider, VStack, ListItem, Skeleton, SkeletonText, useBreakpointValue, useToast } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import useSWR from 'swr'
 import Layout from '../../components/layout'
+import { PriceTag } from '../../components/priceTag'
 
 const Post = () => {
   const router = useRouter()
   const { pid } = router.query
   // @ts-ignore
   const fetcher = (...args) => fetch(...args).then(res => res.json())
-  const { data, error } = useSWR(`/api/getProduct/${pid}`, fetcher)
-  console.log(data, pid)
+  const { data } = useSWR(`/api/getProduct/${pid}`, fetcher)
 
   return <Layout>
     <p>Pid: {pid}, data: {data && data.name}</p>
+    <Container maxW={'7xl'}>
+      <SimpleGrid
+        columns={{ base: 1, lg: 2 }}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 18, md: 24 }}>
+        <Flex>
+          <Skeleton isLoaded={data}>
+            <Image
+              rounded={'md'}
+              alt={'product image'}
+              src={data ? data.imageUrl : '/argon_case.png'}
+              fit={'cover'}
+              align={'center'}
+              w={'100%'}
+              h={{ base: '100%', sm: '400px', lg: '500px' }}
+            />
+          </Skeleton>
+        </Flex>
+        <Stack spacing={{ base: 6, md: 10 }}>
+          <Box as='header'>
+            <Skeleton isLoaded={data}>
+              <Heading
+                lineHeight={1.1}
+                fontWeight={600}
+                fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
+                {data ? data.name : "Loading"}
+              </Heading>
+            </Skeleton>
+            <Skeleton isLoaded={data}>
+              <Text
+                color={useColorModeValue('gray.900', 'gray.400')}
+                fontWeight={300}
+                fontSize={'2xl'}>
+                <PriceTag price={data && data.price} currency="GBP" />
+              </Text>
+            </Skeleton>
+          </Box>
+
+          <Stack
+            spacing={{ base: 4, sm: 6 }}
+            direction={'column'}
+            divider={
+              <StackDivider
+                borderColor={useColorModeValue('gray.200', 'gray.600')}
+              />
+            }>
+            <VStack spacing={{ base: 4, sm: 6 }}>
+              <SkeletonText isLoaded={data} noOfLines={2} spacing={4}>
+                <Text
+                  color={useColorModeValue('gray.500', 'gray.400')}
+                  fontSize={'2xl'}
+                  fontWeight={'300'}>
+                  {data && data.description}
+                </Text>
+              </SkeletonText>
+              <SkeletonText isLoaded={data} noOfLines={4} spacing={4}>
+                <Text fontSize={'lg'}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                  aliquid amet at delectus doloribus dolorum expedita hic, ipsum
+                  maxime modi nam officiis porro, quae, quisquam quos
+                  reprehenderit velit? Natus, totam.
+                </Text>
+              </SkeletonText>
+            </VStack>
+            <Box>
+              <Text
+                fontSize={{ base: '16px', lg: '18px' }}
+                color={useColorModeValue('yellow.500', 'yellow.300')}
+                fontWeight={'500'}
+                textTransform={'uppercase'}
+                mb={'4'}>
+                Features
+              </Text>
+
+              <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+                <List spacing={2}>
+                  <ListItem>Chronograph</ListItem>
+                  <ListItem>Master Chronometer Certified</ListItem>{' '}
+                  <ListItem>Tachymeter</ListItem>
+                </List>
+                <List spacing={2}>
+                  <ListItem>Anti‑magnetic</ListItem>
+                  <ListItem>Chronometer</ListItem>
+                  <ListItem>Small seconds</ListItem>
+                </List>
+              </SimpleGrid>
+            </Box>
+            <Box>
+              <Text
+                fontSize={{ base: '16px', lg: '18px' }}
+                color={useColorModeValue('yellow.500', 'yellow.300')}
+                fontWeight={'500'}
+                textTransform={'uppercase'}
+                mb={'4'}>
+                Product Details
+              </Text>
+
+              <List spacing={2}>
+                <ListItem>
+                  <Text as={'span'} fontWeight={'bold'}>
+                    Between lugs:
+                  </Text>{' '}
+                  20 mm
+                </ListItem>
+                <ListItem>
+                  <Text as={'span'} fontWeight={'bold'}>
+                    Bracelet:
+                  </Text>{' '}
+                  leather strap
+                </ListItem>
+                <ListItem>
+                  <Text as={'span'} fontWeight={'bold'}>
+                    Case:
+                  </Text>{' '}
+                  Steel
+                </ListItem>
+                <ListItem>
+                  <Text as={'span'} fontWeight={'bold'}>
+                    Case diameter:
+                  </Text>{' '}
+                  42 mm
+                </ListItem>
+                <ListItem>
+                  <Text as={'span'} fontWeight={'bold'}>
+                    Dial color:
+                  </Text>{' '}
+                  Black
+                </ListItem>
+                <ListItem>
+                  <Text as={'span'} fontWeight={'bold'}>
+                    Crystal:
+                  </Text>{' '}
+                  Domed, scratch‑resistant sapphire crystal with anti‑reflective
+                  treatment inside
+                </ListItem>
+                <ListItem>
+                  <Text as={'span'} fontWeight={'bold'}>
+                    Water resistance:
+                  </Text>{' '}
+                  5 bar (50 metres / 167 feet){' '}
+                </ListItem>
+              </List>
+            </Box>
+          </Stack>
+
+          <Stack align="center">
+          </Stack>
+        </Stack>
+      </SimpleGrid>
+    </Container>
   </Layout>
 }
 
